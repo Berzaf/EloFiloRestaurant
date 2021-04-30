b7557abbc99fbb9bcc93dd1cc45121cd883aa928
Berzaf <berzafastroo2012@gmail.com>
2021-04-30 12:52:42 -0500

    gallery created

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,163 @@
+import React from 'react';
+import { Fragment } from 'react';
+import { Carousel } from 'react-bootstrap';
+
+
+const Gallery = () => {
+  return (
+    <Fragment>
+            <Carousel fade={true} pause={false}>
+            <Carousel.Item interval={2000}>
+                <img
+                className="d-block w-100"
+                src="/assets/images/breakfast2.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>Foods and Drinks</h3>
+                <p>Enjoy our food and spcial drinks</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={2000}>
+                <img
+                className="d-block w-100"
+                src="/assets/images/specialWine.jpg"
+                alt="Third slide"
+                />
+                <Carousel.Caption>
+                <h3>Foods and Drinks</h3>
+                <p>Enjoy our food and spcial drinks</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={2000}>
+                <img
+                className="d-block w-100"
+                src="/assets/images/coffee.jpg"
+                alt="Third slide"
+                />
+                <Carousel.Caption>
+                <h3>Foods and Drinks</h3>
+                <p>Enjoy our food and spcial drinks</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={2000}>
+                <img
+                className="d-block w-100"
+                src="/assets/images/all_vegtables.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>Foods and Drinks</h3>
+                <p>Enjoy our food and spcial drinks</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item interval={2000}>
+                <img
+                className="d-block w-100"
+                src="/assets/images/burgerFood.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>Foods and Drinks</h3>
+                <p>Enjoy our food and spcial drinks</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item interval={2000}>
+                <img
+                className="d-block w-100"
+                src="/assets/images/specialPizza.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>Foods and Drinks</h3>
+                <p>Enjoy our food and spcial drinks</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item interval={2000}>
+                <img
+                className="d-block w-100"
+                src="/assets/images/specialFoodunsplash.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>Foods and Drinks</h3>
+                <p>Enjoy our food and spcial drinks</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={2000}>
+                <img
+                className="d-block w-100"
+                src="/assets/images/breakfast4.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>Foods and Drinks</h3>
+                <p>Enjoy our food and spcial drinks</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={2000}>
+                <img
+                className="d-block w-100"
+                src="/assets/images/cockDrink.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>Foods and Drinks</h3>
+                <p>Enjoy our food and spcial drinks</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item interval={2000}>
+                <img
+                className="d-block w-100"
+                src="/assets/images/specialJuice.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>Foods and Drinks</h3>
+                <p>Enjoy our food and spcial drinks</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item interval={2000}>
+                <img
+                className="d-block w-100"
+                src="/assets/images/specialWisky.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>Foods and Drinks</h3>
+                <p>Enjoy our food and spcial drinks</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={2000}>
+                <img
+                className="d-block w-100"
+                src="/assets/images/fishFood.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>Foods and Drinks</h3>
+                <p>Enjoy our food and spcial drinks</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={2000}>
+                <img
+                className="d-block w-100"
+                src="/assets/images/lunch.jpg"
+                alt="First slide"
+                />
+                <Carousel.Caption>
+                <h3>Foods and Drinks</h3>
+                <p>Enjoy our food and spcial drinks</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            </Carousel>
+    </Fragment>    
+  )
+}
+export default Gallery;
