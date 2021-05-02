b9c1ee343ab21cb73a03f38b1ac7a7fb39831bbb
Berzaf <70249457+Berzaf@users.noreply.github.com>
2021-05-02 14:48:40 -0500

    file add

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Main from './components/Main/Main';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+
+        <Main/>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
